3bd8221cfae9a6b4adc7c7c064da750f7eb9d45d
WinnySigilai <winnysigilai02@gmail.com>
2023-06-21 14:33:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = new Object()
+    name = "Winny Chelangat",
+
+    streetAddress = "NairobiKenya",
+
+    updateEmployeewithKeyandValue = function (){
+        
+    }
+
+    function updateEmployeeWithKeyAndValue(employee, key, value) {
+        const newObj = {...employee};
+        newObj[key] = value;
+        return newObj;
+    }
+    
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+    }
+    
+    function deleteFromEmployeeByKey(employee, key) {
+        const cloneObj = {...employee};
+        delete cloneObj[key];
+        return cloneObj;
+    }
+    
+    function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key];
+        return employee;
+    }
